1d2bfea251a476c029592cbc69d04ee9b81d0251
Helbis <filiphelbig97@gmail.com>
2017-11-15 22:39:27 +0100

    Update to task 2 -- Final answer

--- Programming17/Exercise5/Ex5--2.c
+++ Programming17/Exercise5/Ex5--2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 /*
@@ -8,11 +9,37 @@ Write a program that verifies
 this hypothesis for the first 20 even numbers.
 */
 
+unsigned int num, p;
 
-int main(){
+
+short int is_prime(int num){
+	if (num <= 1) return 0;
+	if (num % 2 == 0 && num > 2) return 0;
+	    
+	register int i;
+	for(i = 3; i < num / 2; i+= 2){
+		if (num % i == 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+int main(void){
 	printf("\n\n====================\n");
 	
-	
+	printf("Enter a number: ");
+	scanf("%i", &num);
+
+	register int i;
+	for (i=num-2; i > 1; --i){ /* You have to start with two numbers lower or you'll get 1 at some cases*/
+		if (is_prime(i)){
+			p = num - i;
+			printf("\t%d + %d = %d\n", p, i, num);
+			break;
+		}
+	}
 
 	printf("\n====================\n\n");	
 	return 0;
