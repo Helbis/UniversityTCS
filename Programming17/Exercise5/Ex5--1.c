4735f2eb049ad6ddcf36e01fc9934ec2cfe10b7d
Helbis <filiphelbig97@gmail.com>
2017-11-16 21:14:01 +0100

    Final answer to the task

--- Programming17/Exercise5/Ex5--1.c
+++ Programming17/Exercise5/Ex5--1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 
+#define MAX_VAL 1000
+
+
 /*
 1. A twin prime is a prime number that differs
 from another prime number by two.
@@ -15,11 +18,66 @@ or to numbers with the values out of range.
 */
 
 
+int a = MAX_VAL + 1;
+int primes[MAX_VAL];
+int twins[MAX_VAL / 2][2];
 
-int main(){
+
+short int is_prime(int num){
+	if (num <= 1) return 0;
+	if (num % 2 == 0 && num > 2) return 0;
+	    
+	register int i;
+	for(i = 3; i < num / 2; i+= 2){
+		if (num % i == 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+int main(void){
 	printf("\n\n====================\n");
 	
-	
+	char *msg;
+	char *m;
+
+	do{
+		printf("Please enter 'a': ");
+		m = fgets(msg, 80, stdin);
+		sscanf(msg, "%i", &a);
+	}while(a > MAX_VAL);
+
+
+	//Prime numbers calculation
+	int i = 0, j = a, k = 0;
+	while(1){
+		if (is_prime(j) && j > a){
+			primes[i] = j;
+
+			if ((i >= 1) && (primes[i-1] + 2 == primes[i])){
+				twins[k][0] = primes[i - 1];
+				twins[k][1] = primes[i];
+				
+				printf("twin primes: (%i, %i)\n", twins[k][0], twins[k][1]);
+				// printf("\ttwins: (%i, %i)\n", twins[k][0], twins[k][1]);
+				if (k >= 20){
+					break;
+				}
+				
+				k++;
+			}
+			// printf("prime: %i\n", primes[i]);
+			i++;
+		}
+		j++;
+	}
+
+	printf("\n\t~INFO~\n");
+	printf("Number of prime numbers: %i\n", i);
+	printf("Number of prime twins:   %i\n", k);
+
 
 	printf("\n====================\n\n");	
 	return 0;
